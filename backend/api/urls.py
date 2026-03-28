1cbe5c47506617e354ad66bb0b3b85f5e88aaa9f
suday95 <sudaynandan95@gmail.com>
2026-03-29 00:39:34 +0530

    updated backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
         name="category",
     ),
     path("location/", views.LocationView.as_view(), name="location"),
-    path("experiences/", views.ExperienceViewList.as_view(), name="experinence_list"),
+    path("experiences/", views.ExperienceListView.as_view(), name="experinence_list"),
+    path("booking/create/",views.CreateBookingView.as_view(),name="createbooking"),
+    path("payments/create/",views.CreatePaymentView.as_view(),name="createpayment"),# return json file
+    path("payments/create/page/",views.CreatePaymentPageView.as_view(),name="paymentpage"), #returns html page
+    path("payments/verify/",views.VerifyPaymentView.as_view(), name="verifypayment"),
+    path("payments/webhook/", views.RazorpayWebhookView.as_view(), name="razorpay_webhook"),
+
 ]
